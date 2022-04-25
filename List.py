2be962cdf4b2231788b4b65126dd8bb148b6597d
Jitendra Meena <jitendrameena9575@gmail.com>
2022-04-25 20:46:31 +0530

    List Programs

---
+++ List.py
@@ -0,0 +1,21 @@
+
+
+#    @@@ WELLCOME TO LIST PROGRAM @@@@@@
+
+
+# List Append indexing ,slicing, iterarting.....
+
+List =  [1,2,6,3,4]
+
+for i in List:
+    print(i)
+List.append(12)
+List[0]=121
+print(List) 
+
+List.pop()
+
+
+
+
+print(List)
