92899e39a585c6623416e55c39a168dde52478a9
Jitendra Meena <jitendrameena9575@gmail.com>
2022-05-08 16:37:30 +0530

    string count

--- Strings.py
+++ Strings.py
@@ -10,3 +10,18 @@ print(str_list)
 
 for str in range(len(str_list)):
     print(str)
+
+
+# Program to count Upper case and lower Case 
+
+string= 'PythonMeena'
+u_c = 0
+l_c =0
+
+for i in string:
+    if i.isupper():
+        u_c = u_c+1
+    else:
+        l_c = l_c+1
+
+print(u_c,l_c)
