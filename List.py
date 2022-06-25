7edceac4e03a5fc1f3a48a93d7d0a60c7ecef0af
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2022-06-25 23:00:06 +0530

    Update List.py

--- List.py
+++ List.py
@@ -163,4 +163,21 @@ for i in range(l-1):
         new = new +st[i]+str(c)
 print(st)
 print(new)
-    
+
+#Python program to implement matrix addition
+
+X = [[8,5,1],
+[9 ,3,2],
+[4 ,6,3]]
+Y = [[8,5,3],
+[9,5,7],
+[9,4,1]]
+result = [[0,0,0],
+[0,0,0],
+[0,0,0]]
+
+for i in range(len(X)):
+    for j in range(len(Y)):
+        result[i][j]=X[i][j]+Y[i][j]
+for k in result:
+    print(k)
