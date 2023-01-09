74180e85d8889930274725408578489509197830
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2023-01-09 22:25:55 +0530

    count digit programs

--- basic-programs.py
+++ basic-programs.py
@@ -15,4 +15,25 @@ while(n>0):
     n = n//10
 print(r) 
 
+# Python Program to Find Sum of Digits of a Number
+
+n = int(input("Enter a Numbers :-"))
+r = 0
+total = 0
+while(n>0):
+    d = n%10
+    r = r*10+d
+    n = n//10
+    total +=d
+print(total)
+
+
+# Python Program to Count the Number of Digits in a Number
+
+n=int(input("Enter number:"))
+count=0
+while(n>0):
+    count=count+1
+    n=n//10
+print("The number of digits in the number are:",count)
     
