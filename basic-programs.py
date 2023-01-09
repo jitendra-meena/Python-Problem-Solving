61001cf642c1277eac7260a1dde675416918482b
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2023-01-09 21:57:37 +0530

    Basic Programs

--- basic-programs.py
+++ basic-programs.py
@@ -1,1 +1,18 @@
 
+"""
+Python Basic Programs 2023 by Code with Jeet
+
+"""
+
+# Python Program to Reverse a Number
+
+
+n = int(input("Enter a Numbers :-"))
+r = 0
+while(n>0):
+    d = n%10
+    r = r*10+d
+    n = n//10
+print(r) 
+
+    
