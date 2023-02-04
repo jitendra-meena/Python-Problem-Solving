382b4bd4c996a806360fe107bc654cbe63d8f535
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2023-02-04 19:07:34 +0530

    generator functions

--- basic-programs.py
+++ basic-programs.py
@@ -38,3 +38,43 @@ while(n>0):
     n=n//10
 print("The number of digits in the number are:",count)
     
+# Generator in Python 
+'''
+A generator function is a special type of function in Python that returns a generator object.
+ A generator is an iterable object that generates values one at a time,
+ instead of generating a list with all values at once.
+
+'''
+
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+fib = fibonacci_generator()
+for i in range(10):
+    print(next(fib))
+'''
+Output:
+Copy code
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+'''
+'''
+In this example, the fibonacci_generator function returns a generator that generates the Fibonacci sequence.
+The generator is assigned to the variable fib, and we can iterate over it using the for loop or the next function. 
+The function is executed each time we call next(fib), and it stops when it encounters the end of the function.
+'''
+
+
+
+
