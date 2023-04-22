5a8d8bab87f7f7e76e6b2e99c247824b07aae197
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2023-04-22 13:38:01 +0530

    Sum of dict values

--- Dictionary.py
+++ Dictionary.py
@@ -4,3 +4,10 @@ l = [u for u in d]
 l.sort()
 c = {i:d[i] for i in l}
 print(c)  
+
+
+# Python program to find the sum of all items in a dictionary
+
+d = {'Jeet': 10, 'rajnish': 9, 'meena': 15, 'yash': 2, 'kp': 32}
+
+print(sum(d.values()))
